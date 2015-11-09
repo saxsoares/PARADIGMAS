7ffa0b9942d2392884b9b03ff0bd52cf5a583dd1
sax.soares <sax.soares@gmail.com>
2015-11-09 10:35:51 -0200

    Atualizado

--- programa1.hs
+++ programa1.hs
@@ -34,3 +34,92 @@ parImpar lista = [ if not(odd x) then "par" else "impar" | x <- lista ]
 
 combinalistas x y = [ a*b | a<-x, b<-y]
 
+verificaTriangulo::(Int,Int,Int)->String
+verificaTriangulo (a,b,c) = if (a,b,c) `elem` [(x,y,z) | x <- [1..b], y <- [1..c], z<-m, x^2 + y^2 == c^2, x+y+z == 24 ]
+                            then "T"
+                            else "N"
+                            where m = [1..10]
+
+lucky::(Integral a)=>a->String
+lucky 4 = "asd"
+lucky x = "mentira"
+
+fatorial::(Integral a)=>a->a
+fatorial 0 = 1
+fatorial n = n * fatorial(n-1)
+
+integral'::(Integral a)=>a->a
+integral' n = n
+
+integer'::a->a
+integer' n = n
+
+num::(Num a, Integral b)=>b->a
+num a = fromIntegral a
+
+
+number'::(Num a)=>a->String
+number' n  = "n"
+    
+    
+
+tell::(Show a)=>[a]->String
+tell [] = "Lista vazia"
+tell (x:[]) = "Um elemento: " ++ show x
+tell (x:y:[]) = "Dois elementos: " ++ show x ++ ", " ++ show y
+tell (x:y:_) = "grande: " ++ show x ++ ", " ++ show y
+
+funcao::(Show a)=>[a]->String
+funcao xs = case xs of  [] -> "Vazia"
+                        (x:[]) -> "um elemento"
+                        (x:xs) -> "Primeiro elemento " ++ show x
+
+length' xs = sum [1 | _<-xs ]
+
+bmiTell::(RealFloat a)=>a->a->a
+bmiTell peso alt = peso / alt^2
+
+calcBmi::(RealFloat a)=>[(a,a)]->[a]
+calcBmi lista  = [ let bmi = w/h^2 in bmi  | (w, h)<-lista]
+  
+myLast::[a]->a
+myLast [] = error "Lista vazia"
+myLast (x:[]) = x
+myLast (x:xs) = myLast xs
+
+maximum'::(Ord a)=>[a]->a
+maximum' [] = error "Lista vazia"
+maximum'[a] = a
+maximum' (x:xs)
+    | x > maximum' xs = x
+    | otherwise = maximum' xs
+
+replicate'::(Num i, Ord i)=>i->a->[a]
+replicate' n a 
+    |n <= 0 = []
+    |otherwise = a : replicate' (n-1) a
+
+take'::(Num i, Ord i)=>i->[a]->[a]
+take' n _
+    | n <= 0 = []
+take' n [] = []
+take' n (x:xs) = x: take' (n-1) xs
+
+zip'::[a]->[b]->[(a,b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+
+elem'::(Eq a)=>a->[a]->Bool
+elem' _ [] = False
+elem' a (x:xs)
+    | a == x = True
+    | otherwise = elem' a xs
+
+quicksort::(Ord a)=>[a]->[a]
+quicksort [] = []
+quicksort (x:xs) =
+    let left = quicksort [a | a <- xs, a <= x]
+        right = quicksort [a | a <- xs, a > x]
+    in left ++ [x] ++ right    
+    
